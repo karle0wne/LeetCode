fb4ef15fdea379abe9b72e8f347ed4c5730cffe8
Anatoly Karlov <karleowne@gmail.com>
2023-03-05 22:54:33 +0200

    2 Add Two Numbers

--- src/test/kotlin/2_Add_Two_Numbers.kt
+++ src/test/kotlin/2_Add_Two_Numbers.kt
@@ -34,53 +34,30 @@ class `2_Add_Two_Numbers` {
     }
 
     fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
-        return recurs(l1!!, 0, l2!!, ListNode(0)).listNode
+        return round(l1!!, l2!!, 0)
     }
 
-    fun recurs(l1: ListNode, depthL1: Int, l2: ListNode, result: ListNode?): TransferObj {
-        if (l1.next != null) {
-            val transferObj = recurs(l1.next!!, depthL1 + 1, l2, result)
-            return recursl2(l1, depthL1, transferObj.listNode, 0, transferObj.depth - 1, result, false)
-        } else {
-            return recursl2(l1, depthL1, l2, 0, 0, result, true)
-        }
-    }
-
-    fun recursl2(
-        l1: ListNode, depthL1: Int, l2: ListNode, depthL2: Int, limit: Int, result: ListNode?, first: Boolean
-    ): TransferObj {
-        if (l2.next != null && (depthL2 < limit || first)) {
-            val transferObj = recursl2(l1, depthL1, l2.next!!, depthL2 + 1, limit, result, first)
-            return if (transferObj.inc + l2.`val` > 9) {
-                val sum = transferObj.inc + l2.`val`
-                val listNode = ListNode(sum - 10)
-                listNode.next = transferObj.listNode
-                TransferObj(1, listNode, transferObj.depth)
-            } else {
-                val listNode = ListNode(l2.`val`)
-                listNode.next = transferObj.listNode
-                TransferObj(0, listNode, transferObj.depth)
-            }
-        } else if (limit >= 0) {
-            val sum = l1.`val` + l2.`val`
+    fun round(l1: ListNode?, l2: ListNode?, unitItMind: Int): ListNode? {
+        if (l1?.next != null || l2?.next != null) {
+            val sum = (l2?.`val` ?: 0) + (l1?.`val` ?: 0) + unitItMind
             return if (sum > 9) {
-                val listNode = ListNode(sum - 10)
-                listNode.next = l2.next
-                TransferObj(1, listNode, depthL2)
+                val listNode = round(l1?.next, l2?.next, 1)
+                ListNode(sum - 10).apply { next = listNode }
             } else {
-                val listNode = ListNode(sum)
-                listNode.next = l2.next
-                TransferObj(0, listNode, depthL2)
+                val listNode = round(l1?.next, l2?.next, 0)
+                ListNode(sum).apply { next = listNode }
             }
         } else {
-            val listNode = ListNode(l1.`val`)
-            listNode.next = l2
-            return TransferObj(0, listNode, depthL2)
+            val sum = (l2?.`val` ?: 0) + (l1?.`val` ?: 0) + unitItMind
+            return if (sum > 9) {
+                ListNode(sum - 10).apply { next = ListNode(1) }
+            } else {
+                ListNode(sum)
+            }
         }
     }
 }
 
-//  345 3 555555 6
 class ListNode(var `val`: Int) {
     var next: ListNode? = null
 
@@ -88,5 +65,3 @@ class ListNode(var `val`: Int) {
         return `val`.toString() + (next?.toString() ?: "")
     }
 }
-
-class TransferObj(val inc: Int, val listNode: ListNode, val depth: Int)
