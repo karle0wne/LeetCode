e0107905e07b5fec2afe7a240ffec857b1322b8e
Anatoly Karlov <karleowne@gmail.com>
2023-03-06 01:16:43 +0200

    2 Add Two Numbers

--- src/test/kotlin/2_Add_Two_Numbers.kt
+++ src/test/kotlin/2_Add_Two_Numbers.kt
@@ -34,17 +34,17 @@ class `2_Add_Two_Numbers` {
     }
 
     fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
-        return round(l1, l2, 0)
+        return addTwoNumbers(l1, l2, 0)
     }
 
-    fun round(l1: ListNode?, l2: ListNode?, unitItMind: Int): ListNode? {
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?, unitItMind: Int): ListNode? {
         if (l1?.next != null || l2?.next != null) {
             val sum = (l2?.`val` ?: 0) + (l1?.`val` ?: 0) + unitItMind
             return if (sum > 9) {
-                val listNode = round(l1?.next, l2?.next, 1)
+                val listNode = addTwoNumbers(l1?.next, l2?.next, 1)
                 ListNode(sum - 10).apply { next = listNode }
             } else {
-                val listNode = round(l1?.next, l2?.next, 0)
+                val listNode = addTwoNumbers(l1?.next, l2?.next, 0)
                 ListNode(sum).apply { next = listNode }
             }
         } else {
